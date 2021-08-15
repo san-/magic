ea459c3e6ef9bf67934d9eae3c277378d0af060c
Sandro Valgoi <sandro.valgoi@radixeng.com.br>
2021-08-14 21:33:24 -0300

    Spring Start initial project

---
+++ src/main/kotlin/br/save/eti/br/magic/MagicApplication.kt
@@ -0,0 +1,11 @@
+package br.save.eti.br.magic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagicApplication
+
+fun main(args: Array<String>) {
+	runApplication<MagicApplication>(*args)
+}
