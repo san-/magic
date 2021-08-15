2b723c87220d048f1c71bc4137a48627c7d48fff
Sandro Valgoi <sandro.valgoi@radixeng.com.br>
2021-08-15 02:27:55 +0000

    Feign Client

--- src/main/kotlin/br/save/eti/br/magic/MagicApplication.kt
+++ src/main/kotlin/br/save/eti/br/magic/MagicApplication.kt
@@ -2,8 +2,10 @@ package br.save.eti.br.magic
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class MagicApplication
 
 fun main(args: Array<String>) {
